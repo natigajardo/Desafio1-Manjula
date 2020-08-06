8e9882a1fcf56747f1940f4a77e64952be13e1cc
Natalia <natty.gajardo.s@gmail.com>
2020-08-05 23:07:41 -0400

    background and design

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :breast_feedings
   resources :babies
+  root "babies#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
